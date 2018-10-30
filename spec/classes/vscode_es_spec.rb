b0c936ed5f0c95eecc3182d8a02dbd7c4f1dce83
Dries Dams <etxga@rhel-ws6160.emea.agrogroup.net>
2018-10-30 11:31:17 +0100

    update unit tests for PDK compatibility

--- spec/classes/vscode_es_spec.rb
+++ spec/classes/vscode_es_spec.rb
@@ -3,19 +3,24 @@ require 'spec_helper'
 describe 'vscode_es' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      context "minimal install" do
+      context 'minimal install' do
         let(:facts) { os_facts }
-  
+
         it { is_expected.to compile }
-        it { should contain_file('/etc/systemd/system/puppet-languageserver.service').with(
-          :owner => 'root',
-          :group => 'root',
-          :mode  => '0755',
-        )}
-        it { should contain_service('puppet-languageserver').with(
-          :ensure => 'running',
-          :enable => true,
-        )}
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service').with({
+            :owner => 'root',
+            :group => 'root',
+            :mode  => '0755',
+          })
+        end
+        
+        it do
+          is_expected.to contain_service('puppet-languageserver').with({
+            :ensure => 'running',
+            :enable => true,
+          })
+        end
       end
     end
   end
