2acf4b18f61788c635b2d0a7cc816ff003109a40
Dries Dams <etxga@rhel-ws6160.emea.agrogroup.net>
2018-10-30 10:55:03 +0100

    add some extra unit tests

--- spec/classes/vscode_es_spec.rb
+++ spec/classes/vscode_es_spec.rb
@@ -3,9 +3,20 @@ require 'spec_helper'
 describe 'vscode_es' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
-
-      it { is_expected.to compile }
+      context "minimal install" do
+        let(:facts) { os_facts }
+  
+        it { is_expected.to compile }
+        it { should contain_file('/etc/systemd/system/puppet-languageserver.service').with(
+          :owner => 'root',
+          :group => 'root',
+          :mode  => '0755',
+        )}
+        it { should contain_service('puppet-languageserver').with(
+          :ensure => 'running',
+          :enable => true,
+        )}
+      end
     end
   end
 end
