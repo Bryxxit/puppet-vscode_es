184140ce47cebe0bf2fd2253c79b4e2e68637180
Dries Dams <etxga@rhel-ws6160.emea.agrogroup.net>
2018-10-30 15:12:24 +0100

    more rspec refactoring

--- spec/classes/vscode_es_spec.rb
+++ spec/classes/vscode_es_spec.rb
@@ -9,17 +9,17 @@ describe 'vscode_es' do
         it { is_expected.to compile }
         it do
           is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service').with({
-            :owner => 'root',
-            :group => 'root',
-            :mode  => '0755',
-          })
+            'owner' => 'root',
+            'group' => 'root',
+            'mode'  => '0755',
+            })
         end
         
         it do
           is_expected.to contain_service('puppet-languageserver').with({
-            :ensure => 'running',
-            :enable => true,
-          })
+            'ensure' => 'running',
+            'enable' => true,
+            })
         end
       end
     end
