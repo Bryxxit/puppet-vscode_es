ce4efe61fb23cdf95df99091e48e2a1404837bf2
Dries Dams <etxga@rhel-ws6160.emea.agrogroup.net>
2018-10-31 07:59:41 +0100

    trying to solve rubocop linting errors

--- spec/classes/vscode_es_spec.rb
+++ spec/classes/vscode_es_spec.rb
@@ -8,18 +8,18 @@ describe 'vscode_es' do
 
         it { is_expected.to compile }
         it do
-          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service').with({
-            'owner' => 'root',
-            'group' => 'root',
-            'mode'  => '0755',
-            })
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service').with({  
+                                                                                                  'owner' => 'root',
+                                                                                                  'group' => 'root',
+                                                                                                  'mode'  => '0755',
+                                                                                                })
         end
         
         it do
           is_expected.to contain_service('puppet-languageserver').with({
-            'ensure' => 'running',
-            'enable' => true,
-            })
+                                                                        'ensure' => 'running',
+                                                                        'enable' => true,
+                                                                        })
         end
       end
     end
