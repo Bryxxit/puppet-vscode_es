7bcc0745eed49aa25f46943397114cf4957c7453
Dries Dams <etxga@rhel-ws6160.emea.agrogroup.net>
2018-10-31 15:29:46 +0100

    add set of unit tests to module

--- spec/classes/vscode_es_spec.rb
+++ spec/classes/vscode_es_spec.rb
@@ -7,19 +7,99 @@ describe 'vscode_es' do
         let(:facts) { os_facts }
 
         it { is_expected.to compile }
+        it { is_expected.to compile.with_all_deps }
         it do
           is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service').with(
             'owner' => 'root',
             'group' => 'root',
             'mode'  => '0755',
           )
-        end
 
-        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* --timeout=10$/)
+
           is_expected.to contain_service('puppet-languageserver').with(
             'ensure' => 'running',
             'enable' => true,
           )
+
+        end
+      end
+      
+      context 'minimal install failures' do
+        let(:params) { {'debug' => true} }
+        it { is_expected.to compile.and_raise_error(/If debug == true/)}
+      end
+
+      context 'with port => 9654' do
+        let(:params) { {'port' => 9654} }
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* -p 9654$/)
+        end
+      end
+
+      context 'with ipaddr => 0.0.0.0' do
+        let(:params) { {'ipaddr' => '0.0.0.0'}}
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* -i 0.0.0.0$/)
+        end
+      end
+
+      context 'with nostop => true' do
+        let(:params) { {'nostop' => true}}
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* --no-stop$/)
+        end
+      end
+
+      context 'with debug => true' do
+        let(:params) { {'debug' => true, 'debugpath' => '/var/log/puppet/puppet-languageserver' }}
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* --debug=\/var\/log\/puppet\/puppet-languageserver\/debug.log$/)
+        end
+      end
+
+      context 'with slowstart => true' do
+        let(:params) { {'slowstart' => true}}
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* --slow-start$/)
+        end
+      end
+
+      context 'with filecache => true' do
+        let(:params) { {'filecache' => true}}
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* --enable-file-cache$/)
+        end
+      end
+
+      context 'with workspace => /opt/puppetlabs/code' do
+        let(:params) { {'workspace' => '/opt/puppetlabs/code'}}
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* --local-workspace=\/opt\/puppetlabs\/code$/)
+        end
+      end
+
+      context 'with standard set of params' do
+        let(:params) { {'port' => 9654, 'ipaddr' => '0.0.0.0', 'nostop' => true}}
+
+        it do
+          is_expected.to contain_file('/etc/systemd/system/puppet-languageserver.service') \
+            .with_content(/^ExecStart=.* -p 9654 -i 0.0.0.0 --no-stop$/)
         end
       end
     end
