20fbe7296aff4c5fd1dccad048a9522a06adb7fb
damsdri <dries.dams@bryxx.eu>
2019-03-28 12:53:35 +0100

    Get the documentation ready

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,21 +12,17 @@
 # @param ipaddr
 #   IP Address to listen on (0.0.0.0 for all interfaces).
 #   Also, if IPv6 is activated, the editor service will only listen on the IPv6 address by default, if you want it to listen to IPv4
-#   explicitly set this to 0.0.0.0
-#   Default value: 'localhost'
+#   explicitly set this to 0.0.0.0. When no value is set, the value will fall back to `localhost`
 #
 # @param nostop
 #   Do not stop the language server once a client disconnects.
-#   Default value: false
 #
 # @param timeout
 #   Stop the language server if a client does not connection within TIMEOUT seconds.
 #   A value of zero will not timeout.
-#   Default value: 10
 #
 # @param debug
 #   Output debug information.
-#   Default value: false
 #
 # @param debugpath
 #   Either specify a filename or 'STDOUT' as a location for your debug log
@@ -34,11 +30,9 @@
 #
 # @params slowstart
 #   Delay starting the Language Server until Puppet initialisation has completed.
-#   Default value: false
 #
 # @param filecache
 #   Enables the file system cache for Puppet Objects (types, class etc.)
-#   Default value: false
 #
 # @param workspace
 #   The workspace or file path that will be used to provide module-specific functionality.
@@ -46,32 +40,25 @@
 #
 # @param installpath
 #   The location in which the Puppet Editor Services will be installed on your system
-#   Default value: '/opt/puppet-editor-services'
 #
 # @param installuser
 #   User used to execute the bundle install on your system
-#   Default value: 'root'
 #
 # @param installgroup
 #   Group used to execute the bundle install command on the system
-#   Default value: 'root'
 #
 # @param manageservice
 #   enable this to create the necesary configs for your OS to manage the Editor Services as a daemon
-#   Default value: true
 #
 # @param enable
 #   If set, this will make sure that the Editor Services are started after a server reboot
-#   Default value: true
 #
 # @param version
 #   If set, the editor service will be pinned to a specific version of the code. If left blank, the module
 #   will follow the latest release
-#   Default value: undef
 #
 # @example
 #   include vscode_es
-
 class vscode_es(
   Optional[Integer] $port          = undef,
   Optional[String]  $ipaddr        = undef,
